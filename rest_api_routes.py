646b0fc5733a5e95e0260769bdd37043c9ecdd39
eytangro <eytangro@gmail.com>
2021-03-30 08:52:04 +0300

    added some fixes to get and put endpoints

--- rest_api_routes.py
+++ rest_api_routes.py
@@ -16,17 +16,18 @@ def add_user():
 
 
 def get_user():
-    r = requests.get(url=target + '/users/get_user', params=connexion.request.args['user_name'])
+    r = requests.get(url=target + '/users/get_user', params={'user_name': connexion.request.args['user_name']})
+    print(connexion.request.args['user_name'])
     return r.json()
 
 
 def add_friend():
-    r = requests.put(url='/users/add_friend', json=connexion.request.json)
+    r = requests.put(url=target + '/users/add_friend', json=connexion.request.json)
     return r.json()
 
 
 def change_password():
-    r = requests.put(url='users/change_password', json=connexion.request.json)
+    r = requests.put(url=target + '/users/change_password', json=connexion.request.json)
     return r.json()
 
 
@@ -41,17 +42,18 @@ def add_song():
 
 
 def song_upvote():
-    r = requests.put(url='/songs/upvote', json=connexion.request.json)
+    r = requests.put(url=target + '/songs/upvote', json=connexion.request.json)
     return r.json()
 
 
 def song_downvote():
-    r = requests.put(url='/songs/downvote', json=connexion.request.json)
+    r = requests.put(url=target + '/songs/downvote', json=connexion.request.json)
     return r.json()
 
 
 def ranked_songs():
-    r = requests.get(url=target + '/songs/ranked_songs', params=connexion.request.args['user_name'])
+    payload = {'rank': connexion.request.args['rank'], 'op': connexion.request.args['op']}
+    r = requests.get(url=target + '/songs/ranked_songs', params=payload)
     return r.json()
 
 
